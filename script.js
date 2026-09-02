0ff1b15815194533d9514abee277b3fbd0a7fd40
alastairappleton <alastair.appleton+github@gmail.com>
2026-09-02 13:22:32 +0100

    Add new event date for Venue.Tram

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const events = [
   { d: new Date(2026, 5, 3), v: Venue.Tram},
   { d: new Date(2026, 6, 1), v: Venue.Tram},
   { d: new Date(2026, 8, 2), v: Venue.Tram}
+  { d: new Date(2026, 10, 7), v: Venue.Tram}
 ]
 
 
